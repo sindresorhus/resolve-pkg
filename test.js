9d5d5c168efbd80f03a529e2f044a65bd2a03609
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-19 18:15:58 +0100

    init

---
+++ test.js
@@ -0,0 +1,10 @@
+import path from 'path';
+import test from 'ava';
+import fn from './';
+
+test(t => {
+	t.is(fn('nonexistent'), null);
+	t.is(fn('nonexistent/foo'), null);
+	t.is(path.relative('.', fn('grunt-svgmin')), 'node_modules/grunt-svgmin');
+	t.is(path.relative('.', fn('grunt-svgmin/tasks')), 'node_modules/grunt-svgmin/tasks');
+});
